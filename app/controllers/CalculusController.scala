01e499e624b9fef85c3a97e296f8fa602b8172c0
ukieiri <ukieiri@gmail.com>
2017-07-11 09:41:12 +0300

    minor refactoring

--- app/controllers/CalculusController.scala
+++ app/controllers/CalculusController.scala
@@ -9,7 +9,15 @@ import scala.collection.mutable.Stack
 import play.api.libs.json.Json
 import play.api.libs.json._
 import scala.collection.mutable.ArrayBuffer
+import scala.util.control.NonFatal
 
+
+case class TreeNode (v: Int, s: String) {
+  var value: Int = v
+  var symbol: String = s
+  var left: TreeNode = null
+  var right: TreeNode = null
+}
 
 @Singleton
 class CalculusController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {  
@@ -27,12 +35,12 @@ class CalculusController @Inject()(cc: ControllerComponents) extends AbstractCon
        Ok(getResultJson(false, Some(result), null))     
      }
      catch{
-       case e: Throwable => Ok(getResultJson(true, null, e.toString()))
+       case NonFatal(e)=> Ok(getResultJson(true, null, e.toString()))
      }
     }
   } 
   
-  def encodeRawQuery(query: String) : String = {
+  private def encodeRawQuery(query: String) : String = {
     //split in case user sends any other parameters after query
        val queryOnly = query.replace("%20", "").replace("query=", "").split("&")(0)
        val encoder :org.apache.catalina.util.URLEncoder = new org.apache.catalina.util.URLEncoder
@@ -46,36 +54,31 @@ class CalculusController @Inject()(cc: ControllerComponents) extends AbstractCon
        return encoder.encode(queryOnly)      
   }
   
-  def getUTF8Query(query: String) : String = {
+  private def getUTF8Query(query: String) : String = {
     if(query.isEmpty){
       throw new IllegalArgumentException("Query is empty.")   
     }
     val base64RegEx = """^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$""".r   
     val isBase64 = base64RegEx.pattern.matcher(query).matches
-    val string = isBase64 match{
-        case true => return decodeBase64toUTF8(query)
-        case false => checkForIlligalStringinUTF8(query)
-    }      
-    return query
+    //if-else is more efficient for booleans than matching in scala
+    if(isBase64)return decodeBase64toUTF8(query)
+    else checkForIlligalStringinUTF8(query)      
   }
   
-  def checkForIlligalStringinUTF8(query: String) : String = {
+  private def checkForIlligalStringinUTF8(query: String) : String = {
       val QueryRegEx = """[0-9\+\/\*\/\-\s+\()]+""".r   
       val isExpressionOk = QueryRegEx.pattern.matcher(query).matches
-      val string = isExpressionOk match{
-        case true => return query
-        case false => throw new IllegalArgumentException("Invalid input. UTF-8: Please, make sure to use only numbers (0-9) and special characters including +-/*(); Base64: please check the allowed character before encoding.")   
-    }  
-    return query
+      if(isExpressionOk)return query
+      else throw new IllegalArgumentException("Invalid input. UTF-8: Please, make sure to use only numbers (0-9) and special characters including +-/*(); Base64: please check the allowed character before encoding.")
   }
   
-  def decodeBase64toUTF8(expression: String) : String = {
+  private def decodeBase64toUTF8(expression: String) : String = {
     val decoded = Base64.getDecoder.decode(expression)
     val stringQuery = new String(decoded, StandardCharsets.UTF_8)    
     return checkForIlligalStringinUTF8(stringQuery)
   }
   
-  def cleanQuery(expression: String) : ArrayBuffer[String] = {  
+  private def cleanQuery(expression: String) : ArrayBuffer[String] = {  
     //remove all empty spaces between letters 
     val nospaces = expression.replaceAll("\\s+", "")    
     //remove all chars except +-*/() and digits 
@@ -85,13 +88,13 @@ class CalculusController @Inject()(cc: ControllerComponents) extends AbstractCon
     return ArrayBuffer(listofsymbols : _*)
   }  
   
-  def convertToRPNExpression(expression: ArrayBuffer[String]) : ArrayBuffer[String] = {   
+  private def convertToRPNExpression(expression: ArrayBuffer[String]) : ArrayBuffer[String] = {   
     val resultArray = ArrayBuffer[String]()
     val root :TreeNode = createNodeTreeForRPN(expression)
     return getCharactersInRPEOrder(resultArray, root) 
   }
   
-  def calculate(expression: ArrayBuffer[String]) : Double = {
+  private def calculate(expression: ArrayBuffer[String]) : Double = {
     try {
     val stack = Stack[Double]()
     for(token <- expression){
@@ -108,27 +111,15 @@ class CalculusController @Inject()(cc: ControllerComponents) extends AbstractCon
         case "" => //do nothing if empty string
         case _ =>  stack.push(token.toDouble)
       }    
-    } 
-  
+    }   
     return stack.pop()
     }
     catch{
-      case e: Throwable => throw new IllegalArgumentException("Problem with calculations has occurred. Please check if the expression is written correctly.")       
+      case NonFatal(e)=> throw new IllegalArgumentException("Problem with calculations has occurred. Please check if the expression is written correctly.")       
     }
-  }
+  }  
   
-  class TreeNode (v: Int, s: String) {
-    var value: Int = v
-    var symbol: String = s
-    var left: TreeNode = null
-    var right: TreeNode = null
-  }
-  
-  def createNodeTreeForRPN(expression: ArrayBuffer[String]) : TreeNode = {
-    if (expression == null || expression.length == 0) {
-            throw new IllegalArgumentException("Problem with calculations has occurred. Please check if the expression is written correctly.")
-    }
-    
+  private def createNodeTreeForRPN(expression: ArrayBuffer[String]) : TreeNode = {
     val stack = Stack[TreeNode]()
     var base :Int = 0
     var value :Int = 0
@@ -160,9 +151,9 @@ class CalculusController @Inject()(cc: ControllerComponents) extends AbstractCon
         return rst
   }
   
-  def getWeight(base: Int, s: String) : Int = {  
+  private def getWeight(base: Int, s: String) : Int = {  
     if (s == "+" || s == "-") {
-            return base + 1
+        return base + 1
         }
     if (s == "*" || s == "/") {
         return base + 2
@@ -170,10 +161,7 @@ class CalculusController @Inject()(cc: ControllerComponents) extends AbstractCon
     return Integer.MAX_VALUE   
   }
   
-  def getCharactersInRPEOrder(rst: ArrayBuffer[String], node: TreeNode) : ArrayBuffer[String] = {
-    if (node == null) {
-            throw new IllegalArgumentException("Problem with calculations has occurred. Please check if the expression is written correctly.")
-    	}
+  private def getCharactersInRPEOrder(rst: ArrayBuffer[String], node: TreeNode) : ArrayBuffer[String] = {
   	if (node.left != null) {
   	    getCharactersInRPEOrder(rst, node.left)
   	}
@@ -189,15 +177,16 @@ class CalculusController @Inject()(cc: ControllerComponents) extends AbstractCon
     return rst
   } 
   
-    def getResultJson(error :Boolean, result :Some[Double], message :String) : JsValue = {      
-      val rst = error match{
-        case true => Json.obj(
-        "error" -> true,
-        "message" -> message)  
-        case false => Json.obj(
-        "error" -> false,
+  private def getResultJson(error :Boolean, result :Some[Double], message :String) : JsValue = {      
+      if(error) {
+        return Json.obj(
+        "error" -> error,
+        "message" -> message) 
+      }
+      else {
+        return Json.obj(
+        "error" -> error,
         "result" -> result)  
       }
-      return rst
   }
 }
